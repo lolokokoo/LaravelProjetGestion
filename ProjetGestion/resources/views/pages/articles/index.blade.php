17fa008bdfab11ea38f68ed935e4c7f210fab3ee
Mano Raichon <mano.raichon@etu.univ-lyon1.fr>
2023-03-09 14:32:51 +0100

    Boutons articles

--- ProjetGestion/resources/views/pages/articles/index.blade.php
+++ ProjetGestion/resources/views/pages/articles/index.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.master')
 
 @section("contenu")
+    <div class="mt-1">
+        <a href="">
+            <span class="badge badge-primary"><i class="fa fa-plus"></i> Nouvel article</span>
+        </a>
+    </div>
     <div class="mt-3 row">
         @foreach($articles as $article)
             <div class="col-lg-4 col-md-6 col-sm-12">
@@ -12,14 +17,28 @@
                         <div class="col-md-8">
                             <div class="card-body">
                                 <h5 class="card-title font-weight-bold">{{ $article->nom }}</h5>
+                                <div class="float-right">
+                                    <span class="badge badge-dark">
+                                        @if($article->estDisponible)
+                                            Disponible <i class="fa fa-check text-success"></i>
+                                        @else
+                                            Indisponible <i class="fa fa-times text-danger"></i>
+                                        @endif
+                                    </span>
+                                </div>
                                 <p class="card-text">numero de série {{ $article->noSerie }}</p>
-                                <p>
-                                    @if($article->estDisponible)
-                                        <span class="badge badge-success">Disponible </span>
-                                    @else
-                                        <span class="badge badge-danger">Non Disponible </span>
-                                    @endif
-                                </p>
+                                <div class="d-block">
+                                    <div>
+                                        <a href="" class="text-decoration-none">
+                                            <span class="badge badge-warning"><i class="far fa-edit"></i> Modifier</span>
+                                        </a>
+                                    </div>
+                                    <div>
+                                        <a href="">
+                                            <span class="badge badge-danger"><i class="fa fa-trash-alt"></i> Supprimer</span>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
