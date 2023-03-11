b23fc3f187a04a43d632be83ee7e16b1d2bcf286
Mano Raichon <mano.raichon@etu.univ-lyon1.fr>
2023-03-11 17:03:21 +0100

    FactoryLocation -> Date fin en fonction de duree location

--- ProjetGestion/database/factories/LocationFactory.php
+++ ProjetGestion/database/factories/LocationFactory.php
@@ -7,6 +7,7 @@ use App\Models\Client;
 use App\Models\DureeLocation;
 use App\Models\StatutLocation;
 use App\Models\User;
+use DateInterval;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -26,14 +27,35 @@ class LocationFactory extends Factory
         $clients = Client::all();
         $users = User::all();
         $status = StatutLocation::all();
+
+        $dateDebut = $this->faker->dateTimeBetween('-2 month','-1 month');
+        $duree_location_id = array_rand($durees_location->toArray()) + 1;
+        $dateFin = clone $dateDebut;
+
+        switch ($duree_location_id) {
+            case 1:
+                $dateFin->add(new DateInterval('P7D')); // ajoute 7 jours
+                break;
+            case 2:
+                $dateFin->add(new DateInterval('P1D')); // ajoute 1 jour
+                break;
+            case 3:
+                $dateFin->add(new DateInterval('PT12H')); // ajoute 12 heures
+                break;
+            default:
+                $dateFin = $this->faker->dateTimeBetween('-1 month');
+                break;
+        }
+
         return [
-            "dateDebut" => $this->faker->dateTimeBetween('-2 month','-1 month'),
-            "dateFin" => $this->faker->dateTimeBetween('-1 month'),
+            "dateDebut" => $dateDebut,
+            "dateFin" => $dateFin,
             "article_id" => array_rand($articles->toArray()) + 1,
-            "duree_location_id" => array_rand($durees_location->toArray()) + 1,
+            "duree_location_id" => $duree_location_id,
             "client_id" => array_rand($clients->toArray()) + 1,
             "user_id" => array_rand($users->toArray()) + 1,
             "statut_location_id" => array_rand($status->toArray()) + 1,
         ];
     }
+
 }
